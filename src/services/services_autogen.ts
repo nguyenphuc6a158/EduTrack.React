203f9b07a1b40a31a264aa281a8874c2934cf59e
DESKTOP-9PHN3IF\migviet03 <nguyenphuc6a15.8@gmail.com>
2026-04-06 09:47:54 +0700

    gen lại service

--- src/services/services_autogen.ts
+++ src/services/services_autogen.ts
@@ -1326,6 +1326,62 @@ export class ClassService {
     }
 
     /**
+     * @param teacherId (optional) 
+     * @return OK
+     */
+    getClassByTeacher(teacherId: number | undefined, cancelToken?: CancelToken): Promise<ClassDtoPagedResultDto> {
+        let url_ = this.baseUrl + "/api/services/app/Class/GetClassByTeacher?";
+        if (teacherId === null)
+            throw new globalThis.Error("The parameter 'teacherId' cannot be null.");
+        else if (teacherId !== undefined)
+            url_ += "teacherId=" + encodeURIComponent("" + teacherId) + "&";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_: AxiosRequestConfig = {
+            method: "GET",
+            url: url_,
+            headers: {
+                "Accept": "application/json"
+            },
+            cancelToken
+        };
+
+        return this.instance.request(options_).catch((_error: any) => {
+            if (isAxiosError(_error) && _error.response) {
+                return _error.response;
+            } else {
+                throw _error;
+            }
+        }).then((_response: AxiosResponse) => {
+            return this.processGetClassByTeacher(_response);
+        });
+    }
+
+    protected processGetClassByTeacher(response: AxiosResponse): Promise<ClassDtoPagedResultDto> {
+        const status = response.status;
+        let _headers: any = {};
+        if (response.headers && typeof response.headers === "object") {
+            for (const k in response.headers) {
+                if (response.headers.hasOwnProperty(k)) {
+                    _headers[k] = response.headers[k];
+                }
+            }
+        }
+        if (status === 200) {
+            const _responseText = response.data;
+            let result200: any = null;
+            let resultData200  = _responseText;
+            result200 = ClassDtoPagedResultDto.fromJS(resultData200.result);
+            return Promise.resolve<ClassDtoPagedResultDto>(result200);
+
+        } else if (status !== 200 && status !== 204) {
+            const _responseText = response.data;
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+        }
+        return Promise.resolve<ClassDtoPagedResultDto>(null as any);
+    }
+
+    /**
      * @param keyword (optional) 
      * @param skipCount (optional) 
      * @param maxResultCount (optional) 
