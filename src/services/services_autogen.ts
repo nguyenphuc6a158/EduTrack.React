4a39efb0f8cf5f3d486d2bf5d758a9436ac28866
DESKTOP-9PHN3IF\migviet03 <nguyenphuc6a15.8@gmail.com>
2026-04-10 16:50:26 +0700

    gen lại service

--- src/services/services_autogen.ts
+++ src/services/services_autogen.ts
@@ -1571,6 +1571,108 @@ export class ClassService {
     }
 
     /**
+     * @return OK
+     */
+    getAllTeacher( cancelToken?: CancelToken): Promise<UserDtoListResultDto> {
+        let url_ = this.baseUrl + "/api/services/app/Class/GetAllTeacher";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_: AxiosRequestConfig = {
+            method: "GET",
+            url: url_,
+            headers: {
+                "Accept": "application/json"
+            },
+            cancelToken
+        };
+
+        return this.instance.request(options_).catch((_error: any) => {
+            if (isAxiosError(_error) && _error.response) {
+                return _error.response;
+            } else {
+                throw _error;
+            }
+        }).then((_response: AxiosResponse) => {
+            return this.processGetAllTeacher(_response);
+        });
+    }
+
+    protected processGetAllTeacher(response: AxiosResponse): Promise<UserDtoListResultDto> {
+        const status = response.status;
+        let _headers: any = {};
+        if (response.headers && typeof response.headers === "object") {
+            for (const k in response.headers) {
+                if (response.headers.hasOwnProperty(k)) {
+                    _headers[k] = response.headers[k];
+                }
+            }
+        }
+        if (status === 200) {
+            const _responseText = response.data;
+            let result200: any = null;
+            let resultData200  = _responseText;
+            result200 = UserDtoListResultDto.fromJS(resultData200.result);
+            return Promise.resolve<UserDtoListResultDto>(result200);
+
+        } else if (status !== 200 && status !== 204) {
+            const _responseText = response.data;
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+        }
+        return Promise.resolve<UserDtoListResultDto>(null as any);
+    }
+
+    /**
+     * @return OK
+     */
+    getAllStudent( cancelToken?: CancelToken): Promise<UserDtoListResultDto> {
+        let url_ = this.baseUrl + "/api/services/app/Class/GetAllStudent";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_: AxiosRequestConfig = {
+            method: "GET",
+            url: url_,
+            headers: {
+                "Accept": "application/json"
+            },
+            cancelToken
+        };
+
+        return this.instance.request(options_).catch((_error: any) => {
+            if (isAxiosError(_error) && _error.response) {
+                return _error.response;
+            } else {
+                throw _error;
+            }
+        }).then((_response: AxiosResponse) => {
+            return this.processGetAllStudent(_response);
+        });
+    }
+
+    protected processGetAllStudent(response: AxiosResponse): Promise<UserDtoListResultDto> {
+        const status = response.status;
+        let _headers: any = {};
+        if (response.headers && typeof response.headers === "object") {
+            for (const k in response.headers) {
+                if (response.headers.hasOwnProperty(k)) {
+                    _headers[k] = response.headers[k];
+                }
+            }
+        }
+        if (status === 200) {
+            const _responseText = response.data;
+            let result200: any = null;
+            let resultData200  = _responseText;
+            result200 = UserDtoListResultDto.fromJS(resultData200.result);
+            return Promise.resolve<UserDtoListResultDto>(result200);
+
+        } else if (status !== 200 && status !== 204) {
+            const _responseText = response.data;
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+        }
+        return Promise.resolve<UserDtoListResultDto>(null as any);
+    }
+
+    /**
      * @param id (optional) 
      * @return OK
      */
@@ -6322,108 +6424,6 @@ export class UserService {
             return throwException("An unexpected server error occurred.", status, _responseText, _headers);
         }
         return Promise.resolve<boolean>(null as any);
-    }
-
-    /**
-     * @return OK
-     */
-    getAllTeacher( cancelToken?: CancelToken): Promise<UserDtoListResultDto> {
-        let url_ = this.baseUrl + "/api/services/app/User/GetAllTeacher";
-        url_ = url_.replace(/[?&]$/, "");
-
-        let options_: AxiosRequestConfig = {
-            method: "GET",
-            url: url_,
-            headers: {
-                "Accept": "application/json"
-            },
-            cancelToken
-        };
-
-        return this.instance.request(options_).catch((_error: any) => {
-            if (isAxiosError(_error) && _error.response) {
-                return _error.response;
-            } else {
-                throw _error;
-            }
-        }).then((_response: AxiosResponse) => {
-            return this.processGetAllTeacher(_response);
-        });
-    }
-
-    protected processGetAllTeacher(response: AxiosResponse): Promise<UserDtoListResultDto> {
-        const status = response.status;
-        let _headers: any = {};
-        if (response.headers && typeof response.headers === "object") {
-            for (const k in response.headers) {
-                if (response.headers.hasOwnProperty(k)) {
-                    _headers[k] = response.headers[k];
-                }
-            }
-        }
-        if (status === 200) {
-            const _responseText = response.data;
-            let result200: any = null;
-            let resultData200  = _responseText;
-            result200 = UserDtoListResultDto.fromJS(resultData200.result);
-            return Promise.resolve<UserDtoListResultDto>(result200);
-
-        } else if (status !== 200 && status !== 204) {
-            const _responseText = response.data;
-            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
-        }
-        return Promise.resolve<UserDtoListResultDto>(null as any);
-    }
-
-    /**
-     * @return OK
-     */
-    getAllStudent( cancelToken?: CancelToken): Promise<UserDtoListResultDto> {
-        let url_ = this.baseUrl + "/api/services/app/User/GetAllStudent";
-        url_ = url_.replace(/[?&]$/, "");
-
-        let options_: AxiosRequestConfig = {
-            method: "GET",
-            url: url_,
-            headers: {
-                "Accept": "application/json"
-            },
-            cancelToken
-        };
-
-        return this.instance.request(options_).catch((_error: any) => {
-            if (isAxiosError(_error) && _error.response) {
-                return _error.response;
-            } else {
-                throw _error;
-            }
-        }).then((_response: AxiosResponse) => {
-            return this.processGetAllStudent(_response);
-        });
-    }
-
-    protected processGetAllStudent(response: AxiosResponse): Promise<UserDtoListResultDto> {
-        const status = response.status;
-        let _headers: any = {};
-        if (response.headers && typeof response.headers === "object") {
-            for (const k in response.headers) {
-                if (response.headers.hasOwnProperty(k)) {
-                    _headers[k] = response.headers[k];
-                }
-            }
-        }
-        if (status === 200) {
-            const _responseText = response.data;
-            let result200: any = null;
-            let resultData200  = _responseText;
-            result200 = UserDtoListResultDto.fromJS(resultData200.result);
-            return Promise.resolve<UserDtoListResultDto>(result200);
-
-        } else if (status !== 200 && status !== 204) {
-            const _responseText = response.data;
-            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
-        }
-        return Promise.resolve<UserDtoListResultDto>(null as any);
     }
 
     /**
